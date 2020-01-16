a74a87d8175b2dfb8a0a975f560d29a1d20106a8
Kyle Yun <kyle.yun@actwo.com>
2020-01-17 00:07:16 +0900

    결과 데이터 구조 및 Exception

--- src/main/java/com/example/demo/controller/v1/UserController.java
+++ src/main/java/com/example/demo/controller/v1/UserController.java
@@ -1,14 +1,16 @@
 package com.example.demo.controller.v1;
 
 import com.example.demo.entity.User;
+import com.example.demo.model.response.CommonResult;
+import com.example.demo.model.response.ListResult;
+import com.example.demo.model.response.SingleResult;
 import com.example.demo.repo.UserJpaRepo;
+import com.example.demo.service.ResponseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Api(tags = {"1. User"})
 @RequiredArgsConstructor
@@ -16,11 +18,18 @@ import java.util.List;
 @RequestMapping(value = "/v1")
 public class UserController {
     private final UserJpaRepo userJpaRepo;
+    private final ResponseService responseService;
 
-    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
-    @GetMapping(value = "/user")
-    public List<User> findAllUser() {
-        return userJpaRepo.findAll();
+    @ApiOperation(value = "회원 리스트 조회", notes = "모든 회원을 조회한다")
+    @GetMapping(value = "/users")
+    public ListResult<User> findAllUser() {
+        return responseService.getListResult(userJpaRepo.findAll());
+    }
+
+    @ApiOperation(value = "회원 단일 조회", notes = "userId 로 회원을 조회한다")
+    @GetMapping(value = "user/{msrl}")
+    public SingleResult<User> findUserById(@ApiParam(value = "회원ID", required = true) @PathVariable long msrl) {
+        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElse(null));
     }
 
     @ApiOperation(value="회원 입력", notes="회원을 입력한다.")
@@ -32,5 +41,27 @@ public class UserController {
                 .name(name)
                 .build();
         return userJpaRepo.save(user);
+    }
+
+    @ApiOperation(value="회원 수정", notes = "회원정보를 수정한다.")
+    @PutMapping(value = "/user")
+    public SingleResult<User> modify(
+            @ApiParam(value="회원번호", required = true) @RequestParam long msrl,
+            @ApiParam(value="회원아이디", required = true) @RequestParam String uid,
+            @ApiParam(value="회원이름", required = true) @RequestParam String name) {
+        User user = User.builder()
+                .msrl(msrl)
+                .uid(uid)
+                .name(name)
+                .build();
+        return responseService.getSingleResult(userJpaRepo.save(user));
+    }
+
+    @ApiOperation(value = "회원 삭제", notes = "userId로 회원정보를 삭제한다")
+    @DeleteMapping(value = "/user/{msrl}")
+    public CommonResult delete(
+            @ApiParam(value = "회원번호", required = true) @PathVariable long msrl) {
+        userJpaRepo.deleteById(msrl);
+        return responseService.getSuccessResult();
     }
 }
