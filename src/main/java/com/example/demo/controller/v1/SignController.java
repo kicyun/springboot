fb59d51f41209a53a70b0de119d5271b2296f980
Kyle Yun <kyle.yun@actwo.com>
2020-02-11 17:55:06 +0900

    이미 가입한 경우 exception 이 발생해서 실제 오류 상황과 구분되지 않으므로 이미 가입한 경우 먼저 체크해서 로그를 남기자.

--- src/main/java/com/example/demo/controller/v1/SignController.java
+++ src/main/java/com/example/demo/controller/v1/SignController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,7 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+@Slf4j
 @Api(tags = {"1. Sign"})
 @RequiredArgsConstructor
 @RestController
@@ -47,14 +49,24 @@ public class SignController {
     @PostMapping(value = "/signup")
     public CommonResult signup(@ApiParam(value = "회원ID : 이메일", required = true) @RequestParam String id,
                                @ApiParam(value = "비밀번호", required = true) @RequestParam String password,
-                               @ApiParam(value = "이름",required = true) @RequestParam String name) {
-        userService.saveAsync(
-                User.builder()
-                    .uid(id)
-                    .password(passwordEncoder.encode(password))
-                    .name(name)
-                    .roles(Collections.singletonList("ROLE_USER"))
-                    .build());
+                               @ApiParam(value = "이름",required = true) @RequestParam String name) throws ExecutionException, InterruptedException {
+        userService.findByUidAsync(id).thenAcceptAsync(user -> {
+            if (user.isPresent() == true) {
+                // 이미 가입한 가입자의 경우 로그를 남기자
+                log.info("Already Exist User : " + user.get().getUid());
+            } else {
+                // 가입시키자.
+                userService.saveAsync(
+                        User.builder()
+                                .uid(id)
+                                .password(passwordEncoder.encode(password))
+                                .name(name)
+                                .roles(Collections.singletonList("ROLE_USER"))
+                                .build());
+            }
+        });
+
+        // 저장이 되든 말든 성공으로 리턴해서 해커 공격 시 가입여부를 알려주지 않는다.
         return responseService.getSuccessResult();
     }
 }
